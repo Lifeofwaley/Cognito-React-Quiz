e0329a59c2dbef400e99aa6b447a437706ec0937
Adewale Adeoti <lifeofwaley@gmail.com>
2024-09-13 09:02:50 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+test('renders quiz component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const quizElement = screen.getByText(/Question 1/i);
+  expect(quizElement).toBeInTheDocument();
 });
